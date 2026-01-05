6a855477e997b11b0929338bc0a10d0050b00552
Wulfa <66079433+WulfaW@users.noreply.github.com>
2026-01-06 00:30:16 +0300

    backend: ESM için .js uzantılı import düzeltildi

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import session from "express-session";
-import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { registerRoutes } from "./routes.js";
+import { setupVite, serveStatic, log } from "./vite.js";
 import crypto from "crypto";
 
 const app = express();
