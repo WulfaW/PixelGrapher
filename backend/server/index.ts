0edc598353b89d112ae085acd1510b0faf1f3771
Wulfa <66079433+WulfaW@users.noreply.github.com>
2026-01-17 22:57:44 +0300

    Remove runtime import of type definition file

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import session from "express-session";
 import cors from "cors";
-import "./types/express-session.js";
 import { registerRoutes } from "./routes.js";
 import { log } from "./vite.js";
 import crypto from "crypto";
